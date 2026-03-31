819ca61cf7e850114d601a55d59ada96da59f814
osherwin-1 <olive@DESKTOP-7OL15UE>
2026-03-31 19:26:52 -0400

    Add project files.

---
+++ ParkedCar.h
@@ -0,0 +1,22 @@
+#pragma once
+#define PARKEDCAR_H
+#include <string>
+using namespace std;
+
+class ParkedCar {
+private:
+    string make;
+    string model;
+    string color;
+    string licenseNumber;
+    int minutesParked;
+
+public:
+    ParkedCar(const string& mk, const string& mdl, const string& clr, const string& lic, int mins);
+
+    string getMake() const;
+    string getModel() const;
+    string getColor() const;
+    string getLicenseNumber() const;
+    int getMinutesParked() const;
+};
